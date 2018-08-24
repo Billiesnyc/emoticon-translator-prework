f5c4bc0492c75c62a2017045545510686c877755
Billie Simmons <billiesnyc@gmail.com>
2018-08-24 12:58:04 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,8 @@ def load_library(file_path)
   emoticons.each do |name, emoticon|
     library_hash["get_meaning"][emoticon[1]] = name
     library_hash["get_emoticon"][emoticon[0]] = emoticon[1]
+  end
+  library_hash
 end
 
 def get_japanese_emoticon
