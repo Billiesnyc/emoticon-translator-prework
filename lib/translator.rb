d38332fa95b1965f65eaeaf915e9884da36f8daf
Billie Simmons <billiesnyc@gmail.com>
2018-08-24 13:05:43 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,10 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)[get_emoticon][emoticon]
+  if load_library(file_path)[get_emoticon][emoticon]
+    
+  else 
+  
   
 end
 
