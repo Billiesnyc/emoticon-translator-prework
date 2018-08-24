bb38dff65d8eee37db35460f32afb42d9b0b28ec
Billie Simmons <billiesnyc@gmail.com>
2018-08-24 12:54:57 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require "yaml"
 
 def load_library(file_path)
   library_hash = {"get_meaning" => { }, "get_emoticon" => { }}
-  emoticons = 
+  emoticons = YAML.load_file
 end
 
 def get_japanese_emoticon
